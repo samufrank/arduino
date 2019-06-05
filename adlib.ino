b204bb002bb8500ce980df8dc195c1442568c1e6
Sam E <47329201+samufrank@users.noreply.github.com>
2019-06-05 10:48:59 -0700

    Add files via upload

---
+++ adlib.ino
@@ -0,0 +1,32 @@
+#include <LiquidCrystal.h>
+
+
+String places[] = { "closet", "attic", "hospital", "Grandma's house", "bathroom stalls",
+      "market", "Moon", "park", "Student Union", "underground bunker", "forest", "Phoenix College", "bakery",
+      "Pacific Ocean", "basement", "Garfield", "top of a mountain", "ballpark", "pool" };
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+  randomSeed(analogRead(0));
+
+  pinMode(8, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  int reading = digitalRead(8);
+
+  if ( reading == HIGH ) {
+    int index1 = random(0, 18);
+    lcd.clear();
+    lcd.print(places[index1]);
+  }
+
+  delay(50);
+
+}
